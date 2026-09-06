47868aeb4ee0418b6221dd2db3eaaf6291ce3b89
Bchilled <63129967+Bchilled@users.noreply.github.com>
2026-09-06 18:06:57 -0400

    Force both heads back to level 2 until a subclass is chosen

---
+++ ettin/reset.js
@@ -0,0 +1,1 @@
+oLv=2;bLv=2;path.o='';path.b='';pool.o={hp:23,max:23,temp:0};pool.b={hp:20,max:20,temp:0};if(typeof writeHash==='function')writeHash();if(typeof save==='function')save();if(typeof paintAll==='function')paintAll();
