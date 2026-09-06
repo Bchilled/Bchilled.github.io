ca526507221de51f043b1b26259efbc5c9a9b320
Bchilled <63129967+Bchilled@users.noreply.github.com>
2026-09-06 18:07:05 -0400

    Only reset a head that has no subclass pick

--- ettin/reset.js
+++ ettin/reset.js
@@ -1,1 +1,5 @@
-oLv=2;bLv=2;path.o='';path.b='';pool.o={hp:23,max:23,temp:0};pool.b={hp:20,max:20,temp:0};if(typeof writeHash==='function')writeHash();if(typeof save==='function')save();if(typeof paintAll==='function')paintAll();
+if(!path.o){oLv=2;pool.o={hp:23,max:23,temp:0}}
+if(!path.b){bLv=2;pool.b={hp:20,max:20,temp:0}}
+if(typeof writeHash==='function')writeHash();
+if(typeof save==='function')save();
+if(typeof paintAll==='function')paintAll();
